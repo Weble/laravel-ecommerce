eca37a9b2439c1b1b53a773e08db9435dbab4a90
Daniele Rosario <daniele@weble.it>
2023-05-30 10:57:26 +0200

    fix storage manager

--- src/Storage/StorageManager.php
+++ src/Storage/StorageManager.php
@@ -3,12 +3,34 @@
 namespace Weble\LaravelEcommerce\Storage;
 
 use Illuminate\Support\Manager;
+use InvalidArgumentException;
 
 class StorageManager extends Manager
 {
-    public function store(?string $name = null): StorageInterface
+    public function store(?string $driver = null, ?string $name = null): StorageInterface
     {
-        return $this->driver($name);
+        return $this->driver($driver, $name);
+    }
+
+    public function driver($driver = null, $name = null)
+    {
+        $driver = $driver ?: $this->getDefaultDriver();
+        $name = $name ?: $driver;
+
+        if (is_null($driver)) {
+            throw new InvalidArgumentException(sprintf(
+                'Unable to resolve NULL driver for [%s].', static::class
+            ));
+        }
+
+        // If the given driver has not been created before, we will create the instances
+        // here and cache it so we can return it next time very quickly. If there is
+        // already a driver created by this name, we'll just return that instance.
+        if (! isset($this->drivers[$driver][$name])) {
+            $this->drivers[$driver][$name] = $this->createDriver($driver)->setInstanceName($name);
+        }
+
+        return $this->drivers[$driver][$name];
     }
 
     public function getDefaultDriver(): string
